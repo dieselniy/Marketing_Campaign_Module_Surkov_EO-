400852f2f6094132150dc4f013e430634164335c
Egor  Surkov <96268412+dieselniy@users.noreply.github.com>
2026-05-19 14:23:40 +0300

    Изменение пути к файлу с локального на гитовский
    Updated file path for bank.csv to a relative path.

--- pages/Bank Advertising.py
+++ pages/Bank Advertising.py
@@ -7,8 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 from ml_bank_model import pred_client, train_gb_artifact
 from src.auth import init_session_state, require_login, check_session
 
-
-file_path = r"C:\Users\egors\OneDrive\Рабочий стол\GRADUATE\Kaggle Database\bank.csv"
+file_path = "Kaggle Database/bank.csv"
 bank_daf = pds.read_csv(file_path)
 bank_source_columns = bank_daf.columns.tolist()
 init_session_state()
