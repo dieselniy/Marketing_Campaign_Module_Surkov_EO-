b8e6a924b808ee9e26412c8c60838d325697294c
Egor  Surkov <96268412+dieselniy@users.noreply.github.com>
2026-06-06 21:34:12 +0300

    ИМПОРТЫЫ

--- ml_bank_model.py
+++ ml_bank_model.py
@@ -6,7 +6,18 @@ import kagglehub
 from kagglehub import KaggleDatasetAdapter
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    confusion_matrix,
+    f1_score,
+    precision_score,
+    recall_score,
+    roc_auc_score,
+)
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
 
 modelver = "bank_dep_v5"
 
